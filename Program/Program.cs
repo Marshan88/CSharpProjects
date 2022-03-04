8dae73547422df53aa22fe97fd4b1e0c3e53da9c
Marshan88 <mhenrix1@hotmail.com>
2022-03-04 12:34:11 +0100

    Add project files.

---
+++ Program/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Program.Tasks;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What tasks do you want to run?");
+            WriteTasks();
+            Console.Write("Selection ");
+            string taskName = Console.ReadLine();
+            switch (taskName)
+            {
+                case "1":
+                    Console.WriteLine("Task 1 starting");
+                    Task1.Run(args);
+                    break;                
+                
+                case "2":
+                    Console.WriteLine("Task 2 starting");
+                    Task2.Run(args);
+                    break;
+            }
+        }
+
+        static void WriteTasks()
+        {
+            Console.WriteLine("1: Task 1");
+            Console.WriteLine("1: Task 2");
+        }
+    }
+}
---
+++ Program/Tasks/Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.Tasks
+{
+    public class Task1
+    {
+        public static void Run(string[] args)
+        {
+            /*int intVar = 0;
+            string stringVar = "Hello, World!";
+            bool boolVar = true;
+            char charVar = 'A';
+            float floatVar = 3.14f;
+            long longVar = 4153135l;
+            decimal decimalVar = 3.01M;  
+            double doubleVar = 31.21d;*/
+
+            var a = 5;
+            var b = 3.0M;
+            var sum = a + b;           //SVAR: Decimal
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
